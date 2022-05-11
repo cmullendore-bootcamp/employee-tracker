3b245116fa27118bad1a4baa8b906c71f8b40375
Chris Mullendore <chris@vindage.com>
2022-05-11 03:20:21 -0700

    WIP - before refactor for async

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const DB = require("./db/DB");
+const Role = require("./lib/Role");
 
 const db = new DB("localhost", "dbuser", "nothing", 'employees');
 
 db.Initialize();
 
-console.log("connected");
+let role = new Role(db);
+role.Load(1);
+console.log(role);
